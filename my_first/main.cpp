c25d108defee9929cbb11300da7edc6fc588acd2
Bulba Bulbovich <bogdan09109@gmail.com>
2025-11-09 23:11:05 +0300

    added a poor calculator and nuclear watermelon main.cpp
    my firs calculator on cpp :)

--- my_first/main.cpp
+++ my_first/main.cpp
@@ -2,31 +2,51 @@
 
 using namespace std;
 
-int num = 42, a, b;
+float num = 42, a, b;
 auto& sorokdwa = num;
 
 void GimmeSomeTrash(string SayPlease) {
 	if (SayPlease == "Please" or SayPlease == "please") {
 		cout << "yooooo\narbuz\ndesat\nдесять\n" << sorokdwa << endl;
-	}
-	else {
+	} else {
 		cout << "No please, no trash, my friend ;)" << endl;
 	}
 }
 
+void GiMmETheANswErNOOOW(string arbuz, float first, float second) {
+	float answer;
+	if (arbuz == "+") {
+		answer = { first + second };
+	} else if (arbuz == "-") {
+		answer = { first - second };
+	} else if (arbuz == "*") {
+		answer = { first * second };
+	} else if (arbuz == "/") {
+		answer = { first / second };
+	} else {
+		cout << "whaaat" << endl;
+		answer = 42.4242;
+	}
+	cout << "your answer is " << answer << endl;
+}
+
 int main() {
 	setlocale(LC_ALL, "RU");
+
+	string operatatatator;
 	string UserPlease;
+
 	cout << "say please for some trash:"; cin >> UserPlease; cout << endl;
 
 	GimmeSomeTrash(UserPlease);
-	cout << "введи число в переменную A:";
-	cin >> a; cout << endl;
-	cout << "введи число в переменную B:";
-	cin >> b; cout << endl;
+	cout << "введи число в переменную A:"; cin >> a; cout << endl;
+	cout << "введи число в переменную B:"; cin >> b; cout << endl;
 	cout << " A:" << a << " B:" << b << endl;
-	// BREAKING NEWS!!!!!! THIS IS THE END OF WATERMELONS?????? POTATOES STARTS A WAR!!!!!
-	// 🍉 <-- this is watermelon							this is potato army -->🔫🥔🔫🥔🔫🥔
+	cout << "what do you want to do with them? (+|-|*|/):"; cin >> operatatatator; cout << endl;
+	GiMmETheANswErNOOOW(operatatatator, a,b);
+
+	// BREAKING NEWS!!!!!! THIS IS THE END OF POTATOS??????WATERMELON HAS A BOMB!!!!!!
+	// 🍉😈💣💣💣💣 <-- this is watermelon					this is potato army -->🔫🥔🔫🥔🔫🥔
 
 	return 0;
 }
